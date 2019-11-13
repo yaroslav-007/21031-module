6e64051bf848c021a486c0cb42f5ef35dc5e8cf4
yaroslav-007 <43340595+yaroslav-007@users.noreply.github.com>
2019-11-13 13:19:59 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+resource "random_pet" "name" {
+ length    = "13"
+ separator = "-"
+}
+
+
+resource "null_resource" "helloWorld" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name.id}"
+  }
+}
