b5c4ff9ab2d2fa109038adfaf0b7ef5895a9cb65
yaroslav-007 <43340595+yaroslav-007@users.noreply.github.com>
2019-11-13 13:46:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource "null_resource" "helloWorld" {
     command = "echo ${random_pet.name.id}"
   }
 }
+
+resource "null_resource" "helloWorld2" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name.id}"
+  }
+}
